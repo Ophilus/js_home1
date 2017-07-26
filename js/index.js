ace97fd9505662f8241e15b037426043e6a81922
Vadim <vadim-gryn@mail.ru>
2017-07-26 13:27:08 +0000

    edit

--- js/index.js
+++ js/index.js
@@ -1,18 +1,18 @@
 
-let discr;
+let D;
 let x1,x2;
 let a=1,b=1,c=0;
 function discr(){
-    discr = Math.pow(b,2) - 4 * a * c ;
-    return discr;
+    D = Math.pow(b,2) - 4 * a * c ;
+    return D;
 }
 function findRoot(){
-    if(discr == 0){
-        x1 = -b / (2*a);
+    if(D == 0){
+        x1 = (-b + Math.sqrt(D))/(2*a);
         document.getElementById('result').innerHTML='Оскільки D=0, то корінь рівняння лише один:<br><br>  x1 = ' + x1 + ';';
-    }else if(discr > 0){
-        x1 = (-b + Math.sqrt(discr))/(2*a);
-        x2 = (-b - Math.sqrt(discr))/(2*a);
+    }else if(D > 0){
+        x1 = (-b + Math.sqrt(D))/(2*a);
+        x2 = (-b - Math.sqrt(D))/(2*a);
         document.getElementById('result').innerHTML='Корені рівняння:<br><br>  x1 = ' + x1 + ';<br><br>  x2 = ' + x2 + ';';
     }else{
         document.getElementById('result').innerHTML='Оскільки D<0, то коренів рівняння не знайдено';
