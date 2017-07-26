d810b3f94c5f607b94d1d1303e5fc03f0a2a1ddb
Vadim <vadim-gryn@mail.ru>
2017-07-26 13:41:43 +0000

    edit file

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-function discr(a,b,c){
-    let D = Math.pow(b,2) - 4 * a * c ;
+let a, b, c, D, x1, x2;
+function discr(){
+    D = Math.pow(b,2) - 4 * a * c ;
     return D;
 }
-function findRoot(a,b,c,D){
-    let x1, x2;
+function findRoot(){
     if(D == 0){
         x1 = (-b + Math.sqrt(D))/(2*a);
         document.getElementById('result').innerHTML='Оскільки D=0, то корінь рівняння лише один:<br><br>  x1 = ' + x1 + ';';
@@ -16,7 +16,6 @@ function findRoot(a,b,c,D){
     }
 }
 function quadratEquation(){
-    let a, b, c;
     document.getElementById('result').innerHTML='';
     a =parseFloat(document.getElementById('a').value);
     b = parseFloat(document.getElementById('b').value);
